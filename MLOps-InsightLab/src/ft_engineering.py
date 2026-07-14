697b79d22a4079adbdc2515699d26374a873c1ef
Marcela Onofre <marceenau95@gmail.com>
2026-07-14 12:54:23 -0500

    Actualización de ft_engineering, descartando rating y session_duration_bucket de las variables para modelar

--- MLOps-InsightLab/src/ft_engineering.py
+++ MLOps-InsightLab/src/ft_engineering.py
@@ -4,7 +4,6 @@ from sklearn.preprocessing import FunctionTransformer
 from sklearn.compose import ColumnTransformer
 from sklearn.pipeline import Pipeline
 from sklearn.preprocessing import StandardScaler
-from sklearn.preprocessing import OneHotEncoder
 from sklearn.model_selection import train_test_split
 from cargar_datos import cargar_datos
 
@@ -45,12 +44,10 @@ def ft_engineering():
         "pages_viewed",
         "time_on_site_sec",
         "added_to_cart",
-        "rating",
         "payment_method",
         "visit_season",
         "visit_day",
         "visit_month",
-        "session_duration_bucket",
         "location",
         "tiempo_por_pagina",
         "promocion_1",
@@ -60,26 +57,17 @@ def ft_engineering():
     X = df[features] # features
     y = df['purchased']             # target
 
-    # Definimos tipo de variables (numéricas y categóricas)
-    # Variables numéricas (todas menos la categórica)
-    num_features = X.drop('session_duration_bucket', axis=1).columns.tolist()
+    # Definimos tipo de variables (numéricas y categóricas) 
+    num_features = X.columns.tolist()
 
-    # Variable categórica   
-    cat_features = ['session_duration_bucket']
 
     #  Transformación logarítmica solo en discount_amount
     log_transformer = Pipeline(steps=[
         ('log', FunctionTransformer(np.log1p, validate=False)), 
         ])
     # Crear Pipelines
-    # Pipeline 1 : Variables Categóricas
-    cat_transformer = Pipeline(steps=[
-        ('to_str', FunctionTransformer(lambda x: x.astype(str))),
-        ('onehot', OneHotEncoder(handle_unknown='ignore', sparse_output=False))
-    ]
-    )
+    
     # Pipeline 2 : Variables numéricas
-    num_features_no_log = [col for col in num_features if col != 'discount_amount']
     num_transformer =  Pipeline(steps=[
         ('scaler',StandardScaler())
     ]
@@ -90,8 +78,7 @@ def ft_engineering():
     preprocessor = ColumnTransformer(
         transformers=[
             ('num', num_transformer, num_features),
-            ('log_num', log_transformer,['discount_amount']),
-            ('cat', cat_transformer, cat_features)
+            ('log_num', log_transformer,['discount_amount'])
         ]
     )
 
